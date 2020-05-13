83205dd837e8be558be9ba6147b4f3e97cc86cba
Joel Segal <joel.segal@intranda.com>
2020-05-13 09:43:56 +0200

    init

---
+++ tei-import/src/main/java/de/intranda/goobi/plugins/step/tei_import/App.java
@@ -0,0 +1,13 @@
+package de.intranda.goobi.plugins.step.tei_import;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
